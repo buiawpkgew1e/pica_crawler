37ccccb701bcef9108920723e2f583db46c8406b
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2024-01-19 18:39:03 +0800

    1

--- mainchat.py
+++ mainchat.py
@@ -1,108 +1,109 @@
+# encoding: utf-8
 import io
 import json
-import os
 import sys
+import threading
 import traceback
 import logging
-import configparser
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-from util import *
+import os
+import json
+import threading
+import functools
+
 from client import Pica
+from util import *
 
-# 配置日志
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
-# 读取配置文件
-config = configparser.ConfigParser()
-config.read('config.ini')
-DOWNLOAD_PATH = config.get('Paths', 'download_path')
-ZIP_PATH = config.get('Paths', 'zip_path')
-CONCURRENCY = int(config.get('Crawler', 'concurrency'))
-SUBSCRIBE_KEYWORDS = config.get('Subscribe', 'keywords').split(',')
 
-def initialize_pica():
-    p = Pica()
-    p.login()
-    p.punch_in()
-    return p
+# only_latest: true增量下载    false全量下载
+def download_comic(comic, only_latest):
+    """
+    下载漫画
+    :param comic: 漫画信息字典
+    :param only_latest: 是否只下载最新一集
+    """
+    cid = comic["_id"]  # 漫画id
+    title = comic["title"]  # 漫画标题
+    author = comic["author"]  # 漫画作者
+    categories = comic["categories"]  # 漫画分类
+    episodes = p.episodes_all(cid)  # 获取漫画所有话数
 
-def filter_comics(comic, episodes):
-    # 根据需求实现增量更新逻辑
-    pass
-
-def download_picture(p, comic, pics, index, pic_url):
-    try:
-        # ... 实现具体的图片下载逻辑 ...
-        logging.info(f"Downloaded: {pic_url}")
-    except Exception as e:
-        logging.error(f"Download failed for {pic_url}: {e}")
-
-    last = len(pics) if isinstance(index, Exception) else index + 1
-    progress = f"{last}/{len(pics)} ({int(last / len(pics) * 100)}%)"
-    logging.info(f"Download Progress: {progress}")
-
-def download_comic(p, comic, only_latest=False):
-    cid = comic['_id']
-    title = comic['title']
-    author = comic['author']
-    categories = comic['categories']
-
-    logging.info(f"{cid} | {title} | {author} | {categories}| {only_latest}: downloading---------------------")
-
-    episodes = p.episodes_all(cid)
     if only_latest:
-        episodes = filter_comics(comic, episodes)
+        episodes = filter_comics(comic, episodes)  # 筛选需要下载的话数
 
-    pics = []
-    for eid in episodes:
+    if not episodes:
+        return  # 无话数可下载则直接返回
+
+    logging.info(f"{cid} | {title} | {author} | {categories} | {only_latest}: downloading---------------------")
+
+    pics = []  # 图片列表
+    for eid in episodes:  # 遍历每个话数
         page = 1
         while True:
-            docs = json.loads(p.picture(cid, eid['order'], page).content)['data']['pages']['docs']
+            docs = json.loads(p.picture(cid, eid["order"], page).content)["data"]["pages"]["docs"]  # 获取当前话数的图片列表
             page += 1
-            if docs:
-                pics.extend(list(map(lambda i: i['media']['fileServer'] + '/static/' + i['media']['path'], docs)))
+            if docs:  # 若有图片
+                pics.extend(list(
+                    map(lambda i: os.path.join(p.convert_file_name(title), i['media']['path']), docs)))  # 将图片路径添加到列表中
             else:
                 break
 
     if not pics:
-        logging.warning("No pictures found for comic: %s", title)
         return
 
-    path = os.path.join(DOWNLOAD_PATH, convert_file_name(title))
-    os.makedirs(path, exist_ok=True)
+    path = os.path.join('./comics', convert_file_name(title))  # 漫画文件夹路径
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    with ThreadPoolExecutor(max_workers=CONCURRENCY) as executor:
-        future_to_index = {executor.submit(download_picture, p, comic, pics, idx): idx for idx, pic in enumerate(pics)}
-        for future in concurrent.futures.as_completed(future_to_index):
-            index = future_to_index[future]
-            future.result()  # 如果有异常，这里会再次抛出
+    pics_part = list_partition(pics, int(get_cfg('crawl', 'concurrency')))  # 将图片列表按指定数量划分为多个部分
+    for part in pics_part:
+        threads = []
+        for pic in part:
+            pic_path = os.path.join(path, pic)
+            download_partial = functools.partial(download, p, title, pics.index(pic_path), pic_path)
+            t = threading.Thread(target=download_partial)
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+        last = pics.index(part[-1]) + 1  # 计算已下载的图片数量
+        percentage = int(last / len(pics) * 100)
+        logging.info(f"downloaded:{last},total:{len(pics)},progress:{percentage}%")
 
-    with open('downl.txt', 'a') as f:
-        f.write(f'{cid}\n'.encode())
+    with open('./downl.txt', 'ab') as f:
+        f.write((str(cid) + '\n').encode())
 
-def main():
-    p = initialize_pica()
 
-    comics = p.leaderboard()
-    for keyword in SUBSCRIBE_KEYWORDS:
-        subscribe_comics = p.search_all(keyword)
-        logging.info(f'关键词{keyword} : 订阅了{len(subscribe_comics)}本漫画')
-        comics.extend(subscribe_comics)
+p = Pica()
+p.login()
+p.punch_in()
 
-    favourites = p.my_favourite()
+# 排行榜/收藏夹的漫画
+comics = p.leaderboard()
 
-    for comic in favourites + comics:
-        try:
-            logging.info('id | 本子 | 画师 | 分区')
-            download_comic(p, comic, comic not in favourites)
-            info = p.comic_info(comic['_id'])
+# # 关键词订阅的漫画
+keywords = os.environ["SUBSCRIBE_KEYWORD"].split(',')
+for keyword in keywords:
+    subscribe_comics = p.search_all(keyword)
+    print('关键词%s : 订阅了%d本漫画' % (keyword, len(subscribe_comics)))
+    comics += subscribe_comics
 
-            if info['data']['comic']['isFavourite']:
-                p.favourite(comic["_id"])
-        except KeyError as e:
-            logging.error('download failed,%s,%s,%s', comic['_id'], comic["title"], traceback.format_exc())
-            continue
+favourites = p.my_favourite()
+print('id | 本子 | 画师 | 分区')
 
-if __name__ == "__main__":
-    main()
+for comic in favourites + comics:
+    try:
+        # 收藏夹:全量下载  其余:增量下载
+        download_comic(comic, comic not in favourites)
+        info = p.comic_info(comic['_id'])
+        if info["data"]['comic']['isFavourite']:
+            p.favourite(comic["_id"])
+    except:
+        print('download failed,{},{},{}', comic['_id'], comic["title"], traceback.format_exc())
+        continue
+
+# 记录上次运行时间
+f = open('./run_time_history.txt', 'ab')
+f.write((str(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')) + '\n').encode())
+f.close()
