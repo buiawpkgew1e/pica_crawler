30ded2d566fa987d9382f81f4995c14515e8f452
菾凴 <buiawpkgew1@outlook.com>
2024-08-31 14:22:23 +0800

    Update mainchat.py

--- mainchat.py
+++ mainchat.py
@@ -3,20 +3,18 @@ import io
 import json
 import sys
 import threading
+import time
 import traceback
-import logging
+import shutil
 import os
-import json
-import threading
-import functools
+import requests
+from datetime import datetime
 
 from client import Pica
 from util import *
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
-
-# only_latest: true增量下载    false全量下载
 def download_comic(comic, only_latest):
     """
     下载漫画
@@ -29,81 +27,97 @@ def download_comic(comic, only_latest):
     categories = comic["categories"]  # 漫画分类
     episodes = p.episodes_all(cid)  # 获取漫画所有话数
 
+    # 增量更新
     if only_latest:
         episodes = filter_comics(comic, episodes)  # 筛选需要下载的话数
-
     if not episodes:
         return  # 无话数可下载则直接返回
 
-    logging.info(f"{cid} | {title} | {author} | {categories} | {only_latest}: downloading---------------------")
+    print(f'{cid} | {title} | {author} | {categories} | {only_latest}: downloading---------------------', flush=True)  # 打印下载信息
 
     pics = []  # 图片列表
     for eid in episodes:  # 遍历每个话数
         page = 1
         while True:
-            docs = json.loads(p.picture(cid, eid["order"], page).content)["data"]["pages"]["docs"]  # 获取当前话数的图片列表
+            response = p.picture(cid, eid["order"], page)
+            if response.status_code != 200:
+                print(f'Failed to fetch pictures for episode {eid["order"]}', flush=True)
+                break
+            docs = response.json()["data"]["pages"]["docs"]  # 获取当前话数的图片列表
             page += 1
             if docs:  # 若有图片
-                pics.extend(list(
-                    map(lambda i: os.path.join(p.convert_file_name(title), i['media']['path']), docs)))  # 将图片路径添加到列表中
+                pics.extend([i['media']['fileServer'] + '/static/' + i['media']['path'] for i in docs])  # 将图片路径添加到列表中
             else:
                 break
 
-    if not pics:
+    if not pics:  # 若无图片则直接返回
         return
 
     path = os.path.join('./comics', convert_file_name(title))  # 漫画文件夹路径
-    if not os.path.exists(path):
-        os.makedirs(path)
+    os.makedirs(path, exist_ok=True)  # 若文件夹不存在则创建
 
-    pics_part = list_partition(pics, int(get_cfg('crawl', 'concurrency')))  # 将图片列表按指定数量划分为多个部分
-    for part in pics_part:
-        threads = []
-        for pic in part:
-            pic_path = os.path.join(path, pic)
-            download_partial = functools.partial(download, p, title, pics.index(pic_path), pic_path)
-            t = threading.Thread(target=download_partial)
-            threads.append(t)
-            t.start()
-        for t in threads:
-            t.join()
-        last = pics.index(part[-1]) + 1  # 计算已下载的图片数量
-        percentage = int(last / len(pics) * 100)
-        logging.info(f"downloaded:{last},total:{len(pics)},progress:{percentage}%")
+    def download_thread(pic):
+        download(p, title, pics.index(pic), pic)
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=int(get_cfg('crawl', 'concurrency'))) as executor:
+        executor.map(download_thread, pics)
+
+    print(f'Downloaded {len(pics)} images', flush=True)  # 打印下载进度
+
+    # 记录已下载过的id
     with open('./downl.txt', 'ab') as f:
         f.write((str(cid) + '\n').encode())
 
+    # 下载每本漫画的间隔时间
+    if os.environ.get("INTERVAL_TIME"):
+        time.sleep(int(os.environ.get("INTERVAL_TIME")))
 
+# 登录并打卡
 p = Pica()
 p.login()
 p.punch_in()
 
-# 排行榜/收藏夹的漫画
+# 排行榜的漫画
 comics = p.leaderboard()
 
-# # 关键词订阅的漫画
-keywords = os.environ["SUBSCRIBE_KEYWORD"].split(',')
+# 关键词订阅的漫画
+keywords = os.environ.get("SUBSCRIBE_KEYWORD", "").split(',')
 for keyword in keywords:
     subscribe_comics = p.search_all(keyword)
-    print('关键词%s : 订阅了%d本漫画' % (keyword, len(subscribe_comics)))
+    print(f'关键词{keyword} : 订阅了{len(subscribe_comics)}本漫画', flush=True)
     comics += subscribe_comics
 
-favourites = p.my_favourite()
-print('id | 本子 | 画师 | 分区')
+# 收藏夹的漫画
+favourites = p.my_favourite_all()
+print(f'收藏夹共计{len(favourites)}本漫画', flush=True)
+print('id | 本子 | 画师 | 分区', flush=True)
 
 for comic in favourites + comics:
     try:
         # 收藏夹:全量下载  其余:增量下载
         download_comic(comic, comic not in favourites)
         info = p.comic_info(comic['_id'])
+        # 收藏夹中的漫画被下载后,自动取消收藏,避免下次运行时重复下载
         if info["data"]['comic']['isFavourite']:
             p.favourite(comic["_id"])
-    except:
-        print('download failed,{},{},{}', comic['_id'], comic["title"], traceback.format_exc())
+    except Exception as e:
+        print(f'download failed,{comic["_id"]},{comic["title"]},{traceback.format_exc()}', flush=True)
         continue
 
 # 记录上次运行时间
-f = open('./run_time_history.txt', 'ab')
-f.write((str(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')) + '\n').encode())
-f.close()
+with open('./run_time_history.txt', 'ab') as f:
+    f.write((datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n').encode())
+
+# 打包成zip文件, 并删除旧数据 , 删除comics文件夹会导致docker挂载报错
+if os.environ.get("PACKAGE_TYPE", "False") == "True":
+    zip_subfolders('./comics', './output')
+    for filename in os.listdir('./comics'):
+        file_path = os.path.join('./comics', filename)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+# 发送消息通知
+if os.environ.get("BARK_URL"):
+    requests.get(os.environ.get("BARK_URL"))
